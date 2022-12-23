5d9a383c5d5ee6747ce4b9b6ea084db602d18638
Sergei Ragaev <sergei.ragaev@gmail.com>
2022-12-23 14:10:04 +0200

    initial

---
+++ src/Main.java
@@ -0,0 +1,21 @@
+import java.util.Random;
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Введите число от 1 до 100");
+        int value = new Random().nextInt(100);
+        while (true){
+            int attempt = new Scanner(System.in).nextInt();
+            if (attempt == value) {
+                System.out.println("Вы угадали!");
+                break;
+            }
+            if (attempt > value) {
+                System.out.println("Загаданное число меньше");
+            } else {
+                System.out.println("Загаданное число больше");
+            }
+        }
+    }
+}
